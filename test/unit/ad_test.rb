ec39608f3121a8e3763356fe8b9bec50faa45d6e
Rui Gonçalves <ruippeixotog@gmail.com>
2011-11-03 13:28:15 +0000

    Added first test in ads.

--- test/unit/ad_test.rb
+++ test/unit/ad_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class AdTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	# fixtures :users, :ads
+
+	test "favorites" do
+		ad1 = ads(:one)
+		user1 = users(:one)
+
+		assert !(ad1.favorite? user1.id)
+
+		ad1.mark_favorite user1.id
+		assert ad1.favorite? user1.id
+
+		ad1.unmark_favorite user1.id
+		assert !(ad1.favorite? user1.id)
+	end
+
+=begin
+	test "relevance" do
+		ad1 = ads(:one).find
+		ad1 = ads(:two).find
+		user1 = users(:one).find
+
+		assert !ad1.favorite? user1.id
+
+		ad1.mark_favorite user1.id
+		assert ad1.favorite? user1.id
+
+		ad1.unmark_favorite user1.id
+		assert !ad1.favorite? user1.id
+	end
+=end
 end
