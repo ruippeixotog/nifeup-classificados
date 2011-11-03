8b50d454a9af9fdeef80c791ad1021446b5dc61d
Daniel Cibrão <daniel.cibrao@gmail.com>
2011-11-03 17:46:37 +0000

    Facebook upgrade

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -1,13 +1,12 @@
 module AdsHelper
   
   def share_with_facebook_url(opts)
-      url = "http://www.facebook.com/sharer.php?s=100"
+      url = "http://www.facebook.com/sharer.php?"
 
       parameters = []
 
-      opts.each do |k,v|
-        key = "p[#{k}]"
-
+      opts.each do |key,v|
+        
         if v.is_a? Hash
           v.each do |sk, sv|
             parameters << "#{key}[#{sk}]=#{sv}"
