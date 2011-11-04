420403d82b5dbe28d6e840b40e275797e63e31b4
Rui Gonçalves <ruippeixotog@gmail.com>
2011-11-04 01:58:17 +0000

    Added tests for most relevant ads and searching.

--- test/unit/ad_test.rb
+++ test/unit/ad_test.rb
@@ -4,7 +4,7 @@ class AdTest < ActiveSupport::TestCase
 	# fixtures :users, :ads
 
 	test "favorites" do
-		ad1 = ads(:one)
+		ad1 = ads(:a1)
 		user1 = users(:one)
 
 		assert !(ad1.favorite? user1.id)
@@ -17,8 +17,8 @@ class AdTest < ActiveSupport::TestCase
 	end
 
 	test "relevance" do
-		ad1 = ads(:one)
-		ad2 = ads(:two)
+		ad1 = ads(:a1)
+		ad2 = ads(:a2)
 
     ad1_rel = ad1.relevance
     assert ad1_rel > 0
@@ -30,30 +30,61 @@ class AdTest < ActiveSupport::TestCase
     assert ad2_rel > ad1_rel
   end
 
+  # TODO to change when the relevance algorithm is defined
   test "get_most_relevant" do
-    # TODO to change when the relevance algorithm is defined
     arr = Ad.get_most_relevant 3
-    assert arr [ads(:one), ads(:two), ads(:three)]
+    assert_equal arr [ads(:a3), ads(:a2), ads(:a1)]
 
     arr = Ad.get_most_relevant 5
-    assert arr [ads(:one), ads(:two), ads(:three)]
+    assert_equal arr [ads(:a3), ads(:a2), ads(:a1)]
 
     arr = Ad.get_most_relevant 1
-    assert arr [ads(:one)]
+    assert_equal arr [ads(:a3)]
 
     arr = Ad.get_most_relevant 0
-    assert arr []
+    assert_equal arr []
 
     arr = Ad.get_most_relevant -1
-    assert arr nil
+    assert_nil arr
 
     arr = Ad.get_most_relevant nil
-    assert arr nil
+    assert_nil arr
   end
 
   test "search" do
-    # list = Ad.search "porto"
-    # assert ads(:one).section == sections(:two)
-    assert true
+  	# test search in tags
+  	list = Ad.search "Porto"
+    assert_equal list.length 1
+    assert list.include?(ads(:a3))
+    
+  	# test search in a substring of a tag
+  	list = Ad.search "world"
+    assert_equal list.length 1
+    assert list.include?(ads(:a1))
+    
+    # test search in title, case insensivity
+    # and accentuation insensitivity
+    list = Ad.search "universitario"
+    assert_equal list.length 1
+    assert list.include?(ads(:a3))
+    
+    # test multiple results
+    list = Ad.search "FeUp"
+    assert_equal list.length 2
+    assert list.include?(ads(:a2))
+    assert list.include?(ads(:a3))
+    
+    # limit number of results
+    list = Ad.search "FeUp" 1
+    assert_equal list.length 1
+    # TODO to change when the relevance algorithm is defined
+    assert list.include?(ads(:a3)) # by relevance
+    
+    # empty results and invalid inputs
+    list = Ad.search "not in tags"
+    assert list.empty?
+    
+    list = Ad.search nil
+    assert_nil list
   end
 end
