e7d1649cd95fea11986d28192b351327d44c59e1
Rui Gonçalves <ruippeixotog@gmail.com>
2011-11-09 02:17:00 +0000

    Added formatting to 'posted in' date field.

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -28,4 +28,16 @@ module AdsHelper
       url + parameters.join("&")
   end
   
+  def date_format(value)
+		return value.to_i if value.class == String
+		return value if !value.acts_like_time?
+
+		if value.to_date == Date.today
+			return value.strftime "%H:%M"
+		elsif value.year == Date.today.year
+			return value.strftime "%d %B"
+		else
+			return value.strftime "%d %B %Y"
+		end
+	end
 end
