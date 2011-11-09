455e20fff22a0882138762b6521e027d01090186
João Portela <joao.portela@gmail.com>
2011-11-09 22:51:22 +0000

    corrected gallery css for a better display. Set relative paths for raty assets

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,5 @@
 class Resource < ActiveRecord::Base
   belongs_to :ad
-  has_attached_file :link, :styles => { :gallery => "60x60>" }
+  has_attached_file :link, :styles => { :gallery => "60x60" }
+  
 end
