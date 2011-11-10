53cad3bda35d115fad8f5b0c35741ce7969ecd58
Rui Gonçalves <ruippeixotog@gmail.com>
2011-11-10 12:40:14 +0000

    Added comment reporting tests.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,26 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :ad
   has_many :reports
+
+  @@report_limit = 5
+
+  def self.all_reported
+    # TODO get all reported comments
+  end
+
+  def self.set_report_limit(limit)
+    @@report_limit = limit
+  end
+
+  def reported?
+    # TODO return true if the comment has been reported
+  end
+
+  def report!(user_id, reason)
+    # TODO report a comment
+  end
+
+  def badly_reported?    
+    # TODO return true if the comment has been reported more than @@report_limit
+  end
 end
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+		@c1 = comments(:c1)
+	end
+
+  test "report" do
+    user1 = users(:uva)
+
+    assert !@c1.reported?
+    assert comments(:c3).reported?
+    assert_equal [comments(:c3)], Comment.all_reported
+
+    @c1.report! user1.id
+    assert @c1.reported?
+    assert_equal [@c1, comments(:c3)], Comment.all_reported.sort {|c| c.id }
+    assert_raise(AlreadyReportedError) { @c1.report! user1.id }
+
+    @c1.report! users(:ray).id, "Razao do comentario"
+  end
+
+  test "multiple reports" do
+    Comment.set_report_limit 3
+
+    assert !@c1.reported?
+    assert !@c1.badly_reported?
+
+    @c1.report! users(:uva).id
+    assert @c1.reported?
+    assert !@c1.badly_reported?
+
+    @c1.report! users(:ray).id
+    assert @c1.reported?
+    assert !@c1.badly_reported?
+
+    @c1.report! users(:grelhas).id
+    assert @c1.reported?
+    assert @c1.badly_reported?
+  end
 end
