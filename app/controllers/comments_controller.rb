5ce74e0537e07307789a811df4c3743cd6284fa5
Daniel Ferreira <danieljcf@gmail.com>
2011-11-17 11:42:46 +0000

    delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -49,7 +49,7 @@ class CommentsController < ApplicationController
   
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.ad, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.ad, notice:  I18n.t('created_comment') }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
@@ -81,7 +81,7 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to comments_url }
+      format.html { redirect_to @comment.ad, notice: I18n.t('deleted_comment')}
       format.json { head :ok }
     end
   end
