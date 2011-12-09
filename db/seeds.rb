b5fbc21672b307b3e992b7fb47ae029477333bf2
João Portela <joao.portela@gmail.com>
2011-12-09 00:45:21 +0000

    Added seed sections

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Section.create(name: 'Vendo')
+Section.create(name: 'Alugo')
+Section.create(name: 'Troco')
+Section.create(name: 'Procuro')
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
