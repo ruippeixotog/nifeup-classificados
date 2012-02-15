504fbc0bf471b0017a867458e3dde112c52c3afe
Eduardo Pinto <efgpinto@gmail.com>
2012-02-15 23:12:54 +0000

    Feature #1127: 95% - tem que se testar num servidor a serio

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -5,6 +5,6 @@ class Mailer < ActionMailer::Base
     @user = ad.partner
     @ad = ad
     @url  = show_rate_owner_ad_path(@ad.id)
-    mail(:to => @user.email, :subject => I18n.t('site_name') + ": " + I18n.t('subject_email_evaluate_ad'))
+    mail(:to => @user.email, :subject => I18n.t('site_name') + ": " + I18n.t('ad.subject_email_evaluate_ad'))
   end
 end
